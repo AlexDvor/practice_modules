2f8ed2f85220085d8c5956d1f96961a78964f8c3
Alex <alexmatvichuk@gmail.com>
2024-05-23 18:31:38 +0200

    Add Validate Fn + Test

--- react_test/src/__tests__/validatePassword.test.js
+++ react_test/src/__tests__/validatePassword.test.js
@@ -3,9 +3,31 @@ import validatePassword from '../helper/validatePassword'
 import { dummyPasswords } from '../jest.mock/passwords.mock'
 
 describe('validatePassword', () => {
-	it.todo('should return true for a valid password')
-	it.todo('should validate a password against 8 min characters')
-	it.todo('should validate a password against mixed case')
-	it.todo('should validate a password against digits and characters')
-	it.todo('should validate a password against dummy passwords')
+	it('should return true for a valid password', () => {
+		const validPassword = 'MyPassword123!'
+		const expectedResult = validatePassword(validPassword)
+		expect(expectedResult).toBe(true)
+	})
+	it('should validate a password against 8 min characters', () => {
+		const inValidPassword = 'MyPs67!'
+		const expectedResult = validatePassword(inValidPassword)
+		expect(expectedResult).toBe(false)
+	})
+	it('should validate a password against mixed case', () => {
+		const inValidPassword = 'mypassword12!'
+		const inValidPassword2 = 'MYPASSWORD12222!'
+		expect(validatePassword(inValidPassword)).toBe(false)
+		expect(validatePassword(inValidPassword2)).toBe(false)
+	})
+	it('should validate a password against digits and characters', () => {
+		const inValidPassword = 'MyPassword!@'
+		const inValidPassword2 = 'MyPassword#!'
+		expect(validatePassword(inValidPassword)).toBe(false)
+		expect(validatePassword(inValidPassword2)).toBe(false)
+	})
+	it('should validate a password against special character', () => {
+		const inValidPassword = 'MyPassword123'
+		const expectedResult = validatePassword(inValidPassword)
+		expect(expectedResult).toBe(false)
+	})
 })
--- react_test/src/helper/validatePassword.ts
+++ react_test/src/helper/validatePassword.ts
@@ -1,1 +1,16 @@
-export default function validatePassword() {}
+export default function validatePassword(password: string) {
+	if (password.length < 8) {
+		return false
+	}
+	if (!/[A-Z]/.test(password) || !/[a-z]/.test(password)) {
+		return false
+	}
+
+	if (!/[0-9]/.test(password)) {
+		return false
+	}
+	if (!/[^A-Za-z0-9]/.test(password)) {
+		return false
+	}
+	return true
+}
