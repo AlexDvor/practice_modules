ed690f4f80d5a8c523ea5d9b1fea9461b0329983
Alex <alexmatvichuk@gmail.com>
2024-05-26 19:12:50 +0200

    Add Form Test

--- react_test/src/components/Form/Form.test.jsx
+++ react_test/src/components/Form/Form.test.jsx
@@ -1,11 +1,51 @@
 import { fireEvent, render, waitFor } from '@testing-library/react'
+import Form from './Form'
+
+const testId = 'my-child'
 
 describe('Form', () => {
-	it.todo('should render Form with children')
+	it('should render Form with children', () => {
+		const { container, getByTestId } = render(
+			<Form>
+				<div data-testId={testId} />
+			</Form>
+		)
 
-	it.todo('should invoke the onSubmit callback')
+		expect(getByTestId(testId)).toBeInTheDocument()
+		expect(container.querySelector('form')).toBeInTheDocument()
+	})
 
-	it.todo('should invoke the onSuccess callback')
+	it('should invoke the onSubmit callback', () => {
+		const onSubmit = jest.fn()
+		const { container } = render(<Form onSubmit={onSubmit} />)
+		const myForm = container.querySelector('form')
+		fireEvent.submit(myForm)
+		expect(onSubmit).toHaveBeenCalledTimes(1)
+	})
 
-	it.todo('invoke the onError callback')
+	it('should invoke the onSuccess callback', async () => {
+		const onSuccess = jest.fn()
+		const { container } = render(<Form onSubmit={jest.fn()} onSuccess={onSuccess} />)
+		const myForm = container.querySelector('form')
+		fireEvent.submit(myForm)
+		await waitFor(() => {
+			expect(onSuccess).toHaveBeenCalledTimes(1)
+		})
+	})
+
+	it('invoke the onError callback', async () => {
+		const onError = jest.fn()
+
+		const { container } = render(
+			<Form onSubmit={() => Promise.reject()} onError={onError} />
+		)
+
+		const myForm = container.querySelector('form')
+
+		fireEvent.submit(myForm)
+
+		await waitFor(() => {
+			expect(onError).toHaveBeenCalledTimes(1)
+		})
+	})
 })
